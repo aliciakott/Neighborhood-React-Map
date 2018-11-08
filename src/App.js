8b6fabf1e81ea9693aefc78cfcc5badb42b51146
alicia kott <amkott@gmail.com>
2018-11-07 23:11:22 -0500

    places list and markers filter to neighborhoods

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 
 class App extends Component {
   state = {
-    locations : []
+    locations: [],
+    map: {},
+    modalOpen: false,
+    events: []
   }
 
   componentDidMount() {
@@ -19,23 +22,35 @@ class App extends Component {
   }
 
   initMap = () => {
+    let locations = this.state.locations
     let map = new window.google.maps.Map(
       document.getElementById('map'), {
         center: {lat: 39.9526, lng: -75.1652},
         zoom: 13,
         mapTypeControl: false
       })
-    let infowindow = new window.google.maps.Infowindow()
-    this.state.locations.map(location => {
+    let infowindow = new window.google.maps.InfoWindow()
+    let bounds = new window.google.maps.LatLngBounds()
+
+    locations.map(location => {
       let marker = new window.google.maps.Marker({
         position: location.position,
+        animation: window.google.maps.Animation.DROP,
         map: map
       })
-      let content = location.name
+      bounds.extend(marker.position)
       marker.addListener('click', function() {
-        infowindow.setContent(content)
+        infowindow.setContent(location.name)
         infowindow.open(map, marker)
       })
+      location.marker = marker
+      return null
+    })
+    
+    map.fitBounds(bounds);
+    this.setState({
+      locations: locations,
+      map: map
     })
   }
 
@@ -49,23 +64,32 @@ class App extends Component {
   }
 
   filterLocales = (dropmenu) => {
+    this.hideMarkers()
     if (dropmenu === 'all') {
       this.setState({
         locations: locations
-      })
+      }, this.showMarkers)
     } else {
       this.setState({
         locations: locations.filter(location => location.neighborhood === dropmenu)
-      })
+      }, this.showMarkers)
     }
   }
 
   hideMarkers = () => {
-
+    var locations = this.state.locations
+    locations.map(location => location.marker.setMap(null))
+    this.setState({
+      locations: locations
+    })
   }
 
   showMarkers = () => {
-
+    var locations = this.state.locations
+    locations.map(location => location.marker.setMap(this.state.map))
+    this.setState({
+      locations: locations
+    })
   }
 
   render() {
@@ -73,13 +97,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>
-            Kid Friendy Places in Philadelphia
+            Kid Friendy Places in the Greater Philadelphia Area
           </h1>
         </header>
         <main>
           <section id="location-selection">
             <div id="">
-              <select id="" onChange={(event) => this.filterLocales(event.target.value)}>
+              <select aria-label="filter locations by neighborhood" id="neighborhoods-select" onChange={(event) => this.filterLocales(event.target.value)}>
                 <option value="all">All Neighborhoods</option>
                 <option value="center city">Center City</option>
                 <option value="south jersey">South Jersey</option>
@@ -97,7 +121,7 @@ class App extends Component {
           </section>
 
           <section id="map-container">
-            <div id="map"></div>
+            <div id="map" role="application" aria-roledescription="map" aria-label="map of greater philadelphia"></div>
             <div id="added-info"></div>
           </section>
         </main>

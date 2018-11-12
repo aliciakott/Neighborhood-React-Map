ad09a96b531aa64705c91d74097a592663a4ce01
alicia kott <amkott@gmail.com>
2018-11-12 01:11:40 -0500

    foursquare api

--- src/App.js
+++ src/App.js
@@ -49,17 +49,11 @@ class App extends Component {
       })
 
       marker.addListener('click', function() {
-        window.bounceMarker(marker)
+        window.bounceMarker(location)
       })
 
       location.marker = marker
       return null
-    })
-
-    window.google.maps.event.addListener(infowindow, 'domready', function() {
-      document.getElementById('test').addEventListener('click', function(e) {
-        console.log('success!')
-      })
     })
 
     this.setState({
@@ -123,16 +117,55 @@ class App extends Component {
     })
   }
 
-  searchEvents = (location) => {
-    //this.searchEvents(location)
-    var query = null
-    var header = new Headers()
-    var request = new Request(test, { method: 'GET', header })
-    fetch(request).then(response => response.json())
-      .then(data => console.log(data))
+  searchVenue = (location) => {
+    var point = location.position
+    var query = point.lat + ',' + point.lng
+    var venueDetailsURL = null
+    var id = null
+    var url = `https://api.foursquare.com/v2/venues/explore?client_id=QS3H514QHDPTUOALBKTXWKXADRNN4OZBWQCLM5YNMIXZZNFI&client_secret=PCQ2V4XHFQ2K03OTZ522OJ2ZVLARL4KIEBR5M4LJSY1TLTIA&v=20180323&ll=${query}`
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => id = data.response.groups[0].items[0].venue.id)
+      .then(id => {
+        venueDetailsURL = `https://api.foursquare.com/v2/venues/${id}?client_id=QS3H514QHDPTUOALBKTXWKXADRNN4OZBWQCLM5YNMIXZZNFI&client_secret=PCQ2V4XHFQ2K03OTZ522OJ2ZVLARL4KIEBR5M4LJSY1TLTIA&v=20180323`
+        fetch(venueDetailsURL)
+          .then(response => response.json())
+          .then(data => {
+            this.addedInfo(location, data)
+          })
+        })
+        //.catch(error => console.log(error))
+      .catch(error => console.log(error))
+
+  }
+
+  addedInfo = (location, data) => {
+    var locations = this.state.locations
+    var infowindow = this.state.infowindow
+    var venue = data.response.venue
+    var image = venue.bestPhoto
+    infowindow.setContent(`
+      <div id="added-info">
+      <div>${location.name}</div>
+      <div>${venue.hours.status}</div>
+      <div><a href="${location.url}">Visit their website</a></div>
+      <div><img src="${image.prefix + image.width + image.suffix}" alt="${location.name}" height="auto"></div>
+      </div>
+    `)
+
+    infowindow.addListener('closeclick', function() {
+      locations.filter(l => l.name = location.name).map(location => {
+        location.marker.setAnimation(null)
+        infowindow.setContent(null)
+      })
+    })
   }
 
   openMenu = () => {
+    var infowindow = this.state.infowindow
+    infowindow.close()
+    // and set marker animation to null
     this.setState({
       toggleMenu: true
     })
@@ -146,7 +179,8 @@ class App extends Component {
     })
   }
 
-  bounceMarker = (marker) => {
+  bounceMarker = (location) => {
+    var marker = location.marker
     var map = this.state.map
     var infowindow = this.state.infowindow
     infowindow.setContent(null)
@@ -156,22 +190,22 @@ class App extends Component {
       location.marker.setAnimation(null)
       if (location.marker === marker) {
         location.marker.setAnimation(window.google.maps.Animation.BOUNCE)
-        infowindow.setContent(`<div id="test">${location.name}</div>`)
+        infowindow.setContent(`<div>${location.name}</div>`)
         infowindow.open(map, marker)
       }
     })
 
-
     this.setState({
-      locations: locations
-    })
+      locations: locations,
+      toggleMenu: false
+    }, this.searchVenue(location))
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <i class="fas fa-bars" id="menu" onClick={() => this.openMenu()}></i>
+          <i className="fas fa-bars" id="menu" onClick={() => this.openMenu()}></i>
           <h1>
             Kid Friendly Places In NYC
           </h1>
@@ -191,7 +225,7 @@ class App extends Component {
 
               <ul id="list-locales">
                 {this.state.locations.map(location => (
-                  <li key={location.name}><button className="button-link" onClick={() => this.bounceMarker(location.marker)}>{location.name}</button></li>
+                  <li key={location.name}><button className="button-link" onClick={() => this.bounceMarker(location)}>{location.name}</button></li>
                 ))}
               </ul>
             </div>
